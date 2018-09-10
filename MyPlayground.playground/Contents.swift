822c2b95a90b2a5c91712f8db087d2301697e354
hngfu <djsl1018@naver.com>
2018-09-10 18:06:55 +0900

    [구구단] 2,3단 구현
    print()함수로 2,3단을 직접 입력하여 구현하였다.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,6 +2,26 @@
 
 import Cocoa
 
-var str = "Hello, IOS World"
 
-print(str)
+//MARK: - 2, 3단 구현 - 계산과 출력
+var str = "Hello, IOS World"
+//2단
+print("\(2*1)")
+print("\(2*2)")
+print("\(2*3)")
+print("\(2*4)")
+print("\(2*5)")
+print("\(2*6)")
+print("\(2*7)")
+print("\(2*8)")
+print("\(2*9)")
+//3단
+print("\(3*1)")
+print("\(3*2)")
+print("\(3*3)")
+print("\(3*4)")
+print("\(3*5)")
+print("\(3*6)")
+print("\(3*7)")
+print("\(3*8)")
+print("\(3*9)")
