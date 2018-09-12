e35152afc7ce345beb33dbf03c1f6e0846208a93
hngfu <djsl1018@naver.com>
2018-09-12 16:00:48 +0900

    [단위 변환기] 길이 값 입력 및 조건문
    기존에 분류하는 방법대신 prefix suffix를 이용하여 분류했고 readLine()함수를 이용하여 사용자에게 입력받은 값을 이용해 입,출력 하도록 하였습니다.

--- UnitConverter/UnitConverter/main.swift
+++ UnitConverter/UnitConverter/main.swift
@@ -10,8 +10,7 @@ import Foundation
 
 //센치미터(cm)단위를 미터(m)단위로 바꿔주는 함수입니다.
 func centimeterToMeter(cm: Int) -> Float {
-    let convertedCm = Float(cm)/100
-    return convertedCm
+    return Float(cm)/100
 }
 
 //미터(m)단위를 센치미터(cm)단위로 바꿔주는 함수입니다.
@@ -22,18 +21,11 @@ func meterToCentimeter(m: Float) -> Int {
 //MARK: 길이 단위 변환 함수
 //변환할 값을 집어넣으면 변환된 값이 나오게 만들었습니다.
 func unitConverter(_ willConvertData: String) -> String {
-    let rangeOfNumber = ["0","1","2","3","4","5","6","7","8","9","."]
-    var numberOfData = ""
-    var unitOfData = ""
+    let data = willConvertData
+    let startPointOfUnit = data.index(of: "c") ?? data.index(of: "m")
+    let numberOfData = String(data.prefix(upTo: startPointOfUnit!))
+    let unitOfData = String(data.suffix(from: startPointOfUnit!))
 
-    for i in willConvertData {
-        if rangeOfNumber.contains(String(i)) {
-            numberOfData += String(i)
-        } else {
-            unitOfData += String(i)
-        }
-    }
-    
     switch unitOfData {
     case "cm":
         return "\(centimeterToMeter(cm: Int(numberOfData)!))m"
@@ -44,8 +36,9 @@ func unitConverter(_ willConvertData: String) -> String {
     }
 }
 
+if let data = readLine() {
+    print(unitConverter(data))
+} else {
+    print("입력하신 값을 확인해주세요.")
+}
 
-print(unitConverter("183cm"))
-//1.83cm
-print(unitConverter("3.14m"))
-//314cm
