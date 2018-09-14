5ba4c8d64f5a5480f853994bd39e55e44f48e179
hngfu <djsl1018@naver.com>
2018-09-14 15:27:46 +0900

    [단위변환기] 인치 길이 변환과 예외 처리
    함수 분리하고 5단계 완성본

--- UnitConverter/UnitConverter/main.swift
+++ UnitConverter/UnitConverter/main.swift
@@ -10,12 +10,19 @@
 import Foundation
 
 //기본단위(m)를 특정단위로 바꿔주는 함수입니다.
-func convertMeterToUnit(m: Double, to unitValue: Double) -> Double {
-    return m * unitValue
+func converting(what preparedValue: [String]) -> String {
+    //단위의 정보가 담겨있는 곳 입니다.
+    let unitInfo = ["cm" : 100, "m" : 1.0, "inch" : 39.370079]
+    //어떤 값이든 들오면 미터(m)단위로 환산해주고 이제 거기서 다른 단위로 환산해 줍니다.
+    if let preparedNumber = Double(preparedValue[0]), let verifiedUnitBeforeConvert = unitInfo[preparedValue[1]], let verifiedUnitAfterConvert = unitInfo[preparedValue[2]]{
+        let defaultUnitNumber = preparedNumber / verifiedUnitBeforeConvert
+        return "\(defaultUnitNumber * verifiedUnitAfterConvert)\(preparedValue[2])"
+    }
+    return "값을 확인해 주세요."
 }
 
 //예외처리 부분 함수입니다.
-func isSupportable(_ unitBeforeConvert:String, _ unitAfterConvert:String) -> Bool {
+func supportsUnit(_ unitBeforeConvert:String, _ unitAfterConvert:String) -> Bool {
     let unitKey = ["cm","m","inch"]
     if !(unitKey.contains(unitAfterConvert) && unitKey.contains(unitBeforeConvert)){
         return false
@@ -23,52 +30,59 @@ func isSupportable(_ unitBeforeConvert:String, _ unitAfterConvert:String) -> Boo
     return true
 }
 
-//MARK: 인치 길이 변환과 예외 처리
-func unitConverter(_ willConvertData: String) -> String {
-    //숫자부분 단위부분을 분리시켜 줍니다.
-    let convertingData = willConvertData.split(separator: " ").map({String($0)})
+//숫자부분과 단위부분을 분리시켜주는 함수입니다.
+func separateSection(of willSeparateValue:String) -> [String] {
+    let separatedValue = willSeparateValue.split(separator: " ").map({String($0)})
     let rangeOfNumber = ["0","1","2","3","4","5","6","7","8","9","."]
-    var numberOfData = ""
+    var numberSection = ""
     var unitBeforeConvert = ""
-    for i in convertingData[0] {
+    for i in separatedValue[0] {
         if rangeOfNumber.contains(String(i)) {
-            numberOfData += String(i)
+            numberSection += String(i)
         } else {
             unitBeforeConvert += String(i)
         }
     }
     
-    //m와 cm는 변환할 단위가 없어도 변환 가능하게 만들어 줬습니다.
-    var unitAfterConvert = ""
-    if convertingData.count == 2 {
-        unitAfterConvert = convertingData[1]
-    } else if convertingData.count == 1 && unitBeforeConvert == "cm" {
-        unitAfterConvert = "m"
-    } else if convertingData.count == 1 && unitBeforeConvert == "m" {
-        unitAfterConvert = "cm"
+    if separatedValue.count == 2 {
+        return [numberSection,unitBeforeConvert,separatedValue[1]]
     }
+    return [numberSection,unitBeforeConvert]
+}
+
+//변환할 단위를 입력하지 않은 경우의 처리
+func processWithoutUnitsToConvert(valueWithoutUnitsToConvert:[String]) -> [String] {
+    var valueToProcess = valueWithoutUnitsToConvert
+    if valueToProcess[1] == "cm" {
+        valueToProcess.append("m")
+    } else if valueToProcess[1] == "m" {
+        valueToProcess.append("cm")
+    }
+    return valueToProcess
+}
+
+
+//MARK: 인치 길이 변환과 예외 처리
+func convertUnit(_ willConvertValue: String) -> String {
+    //숫자부분 단위부분을 분리시켜 줍니다.
+    let seperatedValues = separateSection(of: willConvertValue)
     
-    //단위의 정보가 담겨있는 곳 입니다.
-    let unitInfo = ["cm" : 100, "m" : 1, "inch" : 39.370079]
+    //m와 cm는 변환할 단위가 없어도 변환 가능하게 만들어 줬습니다.
+    let preparedValue = processWithoutUnitsToConvert(valueWithoutUnitsToConvert: seperatedValues)
     
     //예외처리해주고 알맞은 값은 단위를 변환시켜줍니다.
-    if isSupportable(unitBeforeConvert, unitAfterConvert) {
-        let defaultUnitNumber = Double(numberOfData)! / unitInfo[unitBeforeConvert]!
-        return "\(convertMeterToUnit(m: defaultUnitNumber, to: unitInfo[unitAfterConvert]!))\(unitAfterConvert)"
+    if supportsUnit(preparedValue[1], preparedValue[2]) {
+        return converting(what: preparedValue)
     } else {
         return "지원하지 않는 단위입니다. 단위를 확인해 주세요."
     }
-    
 }
 
-if let data = readLine() {
-    print(unitConverter(data))
+if let valueAndUnit = readLine() {
+    print(convertUnit(valueAndUnit))
 } else {
     print("입력하신 값을 확인해주세요.")
 }
-
-
-
 
 
 
