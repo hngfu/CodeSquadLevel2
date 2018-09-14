4019597a82dc547b71815da5e432d23e49c51c6b
hngfu <djsl1018@naver.com>
2018-09-14 10:21:28 +0900

    [단위변환기] 인치 길이 변환과 예외 처리
    before after 이 들어간 변수 중에 전치사 뒤에 convert를 붙여줌

--- UnitConverter/UnitConverter/main.swift
+++ UnitConverter/UnitConverter/main.swift
@@ -19,36 +19,37 @@ func unitConverter(_ willConvertData: String) -> String {
     let convertingData = willConvertData.split(separator: " ").map({String($0)})
     let rangeOfNumber = ["0","1","2","3","4","5","6","7","8","9","."]
     var numberOfData = ""
-    var unitBefore = ""
+    var unitBeforeConvert = ""
     for i in convertingData[0] {
         if rangeOfNumber.contains(String(i)) {
             numberOfData += String(i)
         } else {
-            unitBefore += String(i)
+            unitBeforeConvert += String(i)
         }
     }
-    var unitAfter = "doNotInput"
+    
+    var unitAfterConvert = "doNotInput"
     if convertingData.count == 2 {
-        unitAfter = convertingData[1]
+        unitAfterConvert = convertingData[1]
     }
     let unitInfo = ["cm":100, "m":1, "inch":39.370079]
     
     
     //예외처리 부분
     let unitKey = ["cm","m","inch"]
-    if (!unitKey.contains(unitAfter) || !unitKey.contains(unitBefore)) && unitAfter != "doNotInput" {
+    if (!unitKey.contains(unitAfterConvert) || !unitKey.contains(unitBeforeConvert)) && unitAfterConvert != "doNotInput" {
         return "지원하지 않는 단위입니다. 단위를 확인해 주세요."
     }
     
-    let defaultNumber = Double(numberOfData)! / unitInfo[unitBefore]!
+    let defaultNumber = Double(numberOfData)! / unitInfo[unitBeforeConvert]!
     
     //m와 cm는 변환할 단위 입력 안해되게 구현
-    if unitBefore == "m" && unitAfter == "doNotInput" {
+    if unitBeforeConvert == "m" && unitAfterConvert == "doNotInput" {
         return "\(convertCentimeterToUnit(m: defaultNumber, to: unitInfo["cm"]!))cm"
-    } else if unitBefore == "cm" && unitAfter == "doNotInput" {
+    } else if unitBeforeConvert == "cm" && unitAfterConvert == "doNotInput" {
         return "\(convertCentimeterToUnit(m: defaultNumber, to: unitInfo["m"]!))m"
     } else {
-        return "\(convertCentimeterToUnit(m: defaultNumber, to: unitInfo[unitAfter]!))\(unitAfter)"
+        return "\(convertCentimeterToUnit(m: defaultNumber, to: unitInfo[unitAfterConvert]!))\(unitAfterConvert)"
     }
     
 }
